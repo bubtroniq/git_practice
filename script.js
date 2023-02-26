5feb7eb462bd2abd759e3da5a862022be9b5b1ea
bubtroniq <joitescurazvan@gmail.com>
2023-02-26 20:15:57 +0000

    Add flow control practice code

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-console.log("Hai");
+// console.log("Hai");
 
-let boolean = false;
-let n = null;
-
-
+// let boolean = false;
+// let n = null;
 
 
-console.log(typeof(boolean));
-console.log(typeof(n));
+
+
+// console.log(typeof(boolean));
+// console.log(typeof(n));
+
+
+let n = Math.floor(Math.random() * 100) + 1;
+console.log(n);
+
+if (n < 35) {
+    prompt("Your love score is low");
+} else if (n > 35 && n < 75) {
+    prompt("Your love score is good");
+} else {
+    prompt("Your love score is super");
+}
